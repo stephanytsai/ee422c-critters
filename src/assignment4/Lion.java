56942838e5f410da773150368a63afc42512d92a
Rajan Makanji <rajanmakanji@gmail.com>
2016-10-05 16:37:13 -0500

    created 2 new critters, tiger and lion

---
+++ src/assignment4/Lion.java
@@ -0,0 +1,17 @@
+package assignment4;
+
+public class Lion extends Critter {
+
+	@Override
+	public void doTimeStep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean fight(String oponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
