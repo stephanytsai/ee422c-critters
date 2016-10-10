1570bd08f9f98597cbb06ef7882a17eeb11fa7ea
Rajan Makanji <rajanmakanji@gmail.com>
2016-10-10 09:20:58 -0500

    Edit Lion and Tiger Critter subclasses with abstract toString method

--- src/assignment4/Lion.java
+++ src/assignment4/Lion.java
@@ -1,11 +1,18 @@
 package assignment4;
 
 public class Lion extends Critter {
-
+	
+	// Represent the Lion Critter subclass with the char "L"
+	@Override
+	public String toString() {
+		return "L";
+	}
+	
+	// Lion will walk in direction 4 and use amt of energy in walk_energy_cost
 	@Override
 	public void doTimeStep() {
 		// TODO Auto-generated method stub
-
+		this.walk(4);	
 		
 	}
 
--- src/assignment4/Tiger.java
+++ src/assignment4/Tiger.java
@@ -1,10 +1,18 @@
 package assignment4;
 
 public class Tiger extends Critter{
-
+	
+	@Override
+	public String toString() {
+		return "T";
+	}
+	
+	
+	// Runs in direction 7 and uses amt of energy in run_energy_cost
 	@Override
 	public void doTimeStep() {
 		// TODO Auto-generated method stub
+		this.run(7); 
 		
 	}
 
